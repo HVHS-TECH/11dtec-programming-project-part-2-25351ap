5bc587059093759f766057f8c05a93e61c6dd352
25351ap <25351ap@hvhs.school.nz>
2025-06-26 00:48:52 +0000

    Jun 26, 2025, 12:48 AM

--- bobsrentals.js
+++ bobsrentals.js
@@ -18,28 +18,28 @@ let numSeatArray = ["null", "1", "2", "3", "4", "5", "6", "7", "8"];
 function processInput() {
     let seat = document.forms[place_user_order][user_input_seat].value;
     if (seat == numSeatArray[1]) {
-        alert("The car with one seat that we recomend for you is the " + carsArray[1]);
+        test.innerHTML = "The car with one seat that we recomend for you is the " + carsArray[1];
     }
     else if (seat == numSeatArray[2]) {
-        alert("The car with two seats that we recomend for you is the " + carsArray[2]);
+        test.innerHTML = "The car with two seats that we recomend for you is the " + carsArray[2];
     }
     else if (seat == numSeatArray[3]) {
-        alert("The car with three seats that we recomend for you is the " + carsArray[3]);
+        test.innerHTML = "The car with three seats that we recomend for you is the " + carsArray[3];
     }
     else if (seat == numSeatArray[4]) {
-        alert("The car with four seats that we recomend for you is the " + carsArray[4]);
+        test.innerHTML = "The car with four seats that we recomend for you is the " + carsArray[4];
     }
     else if (seat == numSeatArray[5]) {
-        alert("The car with five seats that we recomend for you is the " + carsArray[5]);
+        test.innerHTML = "The car with five seats that we recomend for you is the " + carsArray[5];
     }
     else if (seat == numSeatArray[6]) {
-        alert("The car with six seats that we recomend for you is the " + carsArray[6]);
+        test.innerHTML = "The car with six seats that we recomend for you is the " + carsArray[6];
     }
     else if (seat == numSeatArray[7]) {
-        alert("The car with seven seats that we recomend for you is the " + carsArray[7]);
+        test.innerHTML = "The car with seven seats that we recomend for you is the " + carsArray[7];
     }
     else if (seat == numSeatArray[8]) {
-        alert("The car with eight seats that we recomend for you is the " + carsArray[8]);
+        test.innerHTML = "The car with eight seats that we recomend for you is the " + carsArray[8];
     }
 }
 
