3fc89221d74d9aac3711ab086056a449d17e7571
25351ap <25351ap@hvhs.school.nz>
2025-06-27 03:15:26 +0000

    Added form

--- bobsrentals.js
+++ bobsrentals.js
@@ -6,10 +6,9 @@ console.log("Running Bobrentals.js");
 //Variables
 let userName;
 let userNameInvalid = true;
-let numSeat;
-let numSeatInvalid = true;
-let carsArray = ["null", "BAC Mono", "MX5", "Mini", "Falcon", "Outlander", "Estima"];
+let carsArray = ["null", "BAC Mono", "MX5", "Mini", "i30", "Falcon", "Kona", "Prado", "Outlander", "Estima"];
 let numSeatArray = ["null", "1", "2", "3", "4", "5", "6", "7", "8"];
+let carPriceArray = ["null", "25", "30", "40", "50", "60", "70"]
 
 /****************************
  Main code
@@ -18,33 +17,25 @@ let numSeatArray = ["null", "1", "2", "3", "4", "5", "6", "7", "8"];
 function processInput() {
     console.log("testing");
     let seat = user_input_seat.value;
-    let day = input_num_day.value;
     if (seat == numSeatArray[1]) {
-        test.innerHTML = "Based on your requirements we recomend the " + carsArray[1] + " . The " + carsArray[1] + " is $25 a day";
+        test.innerHTML = "Based on your requirements we recomend the " + carsArray[1] + " . The " + carsArray[1] + " is " + carPriceArray[1] + " dollars a day with out insurance. The " + carsArray[1] + " has one seat.";
     }
     else if (seat == numSeatArray[2]) {
-        test.innerHTML = "The car with two seats that we recomend for you is the " + carsArray[2];
+        test.innerHTML = "Based on your requirements we recomend the " + carsArray[2] + " . The " + carsArray[2] + " is " + carPriceArray[2] + " dollars a day with out insurance. The " + carsArray[2] + " has two seats.";
     }
-    else if (seat == numSeatArray[3]) {
-        test.innerHTML = "The car with three seats that we recomend for you is the " + carsArray[3];
-    }
-    else if (seat == numSeatArray[4]) {
-        test.innerHTML = "The car with four seats that we recomend for you is the " + carsArray[4];
+    else if (seat == numSeatArray[4] || seat == numSeatArray[3]) {
+        test.innerHTML = "Based on your requirements we recomend the " + carsArray[3] + " and the " + carsArray[4] + " . The " + carsArray[3] + " and the " + carsArray[4] + " are both " + carPriceArray[3] + " dollars a day with out insurance. Both cars have four seats.";
     }
     else if (seat == numSeatArray[5]) {
-        test.innerHTML = "The car with five seats that we recomend for you is the " + carsArray[5];
+        test.innerHTML = "Based on your requirements we recomend the " + carsArray[5] + ", " + carsArray[6] + " and the " + carsArray[7] + " . The " + carsArray[5] + ", " + carsArray[6] + " and the " + carsArray[7] + " are all " + carPriceArray[4] + " dollars a day with out insurance. All three cars have five seats.";
     }
-    else if (seat == numSeatArray[6]) {
-        test.innerHTML = "The car with six seats that we recomend for you is the " + carsArray[6];
-    }
-    else if (seat == numSeatArray[7]) {
-        test.innerHTML = "The car with seven seats that we recomend for you is the " + carsArray[7];
+    else if (seat == numSeatArray[7] || seat == numSeatArray[6]) {
+        test.innerHTML = "Based on your requirements we recomend the " + carsArray[8] + " . The " + carsArray[8] + " is " + carPriceArray[5] + " dollars a day with out insurance. The " + carsArray[8] + " has 7 seats.";
     }
     else if (seat == numSeatArray[8]) {
-        test.innerHTML = "The car with eight seats that we recomend for you is the " + carsArray[8];
+        test.innerHTML = "Based on your requirements we recomend the " + carsArray[9] + " . The " + carsArray[9] + " is " + carPriceArray[6] + " dollars a day with out insurance. The " + carsArray[9] + " has 8 seats.";
     }
 }
-
  /****************************
  Functions
  ****************************/
@@ -58,18 +49,6 @@ function askUserName() {
         else {
             userNameInvalid = false;
             welcome.innerHTML = "Welcome, to Bob's car rentals " + userName + " . To place an order click the green button.";
-        }
-    }
-}
-//function asking user how many seats they want
-function askUserNumberSeats() {
-    numSeat = prompt ("How many seats would you like your rental car to have? \nPlease enter a number between and including 1-8.");
-    while (numSeatInvalid) {
-        if (numSeat == "" || numSeat == " " || (isNaN(numSeat)) || numSeat >= 1 || numSeat <= 8) {
-            numSeat = prompt ("ERROR! \nInvalid number of seats entered. \nPlease enter a valid number of seats between and including 1-8.");
-        }
-        else {
-            numSeatInvalid = false;
         }
     }
 }
